410171f6595b26cab2fca2c04297075099634534
Kawin Thimayom <kawinhill1112@gmail.com>
2023-10-25 15:34:11 +0700

    add build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+export MESA_GLSL_VERSION_OVERRIDE=330
+export MESA_GL_VERSION_OVERRIDE=3.3
+export PLATFORM=PLATFORM_DESKTOP
+export GRAPHICS=GRAPHICS_API_OPENGL_21
+# sudo apt install xorg-dev -y
+
+cmake -S . -B build
+cd build
+rm -rf my_raylib_game
+rm -rf CMakeFiles/my_raylib_game.dir/sources/*.o
+make
+./my_raylib_game
